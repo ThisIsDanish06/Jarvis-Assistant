87cd09ae376b26d8ef02a3cf4f98980c60eb21ee
Danish Ansari <ansaridanish78678@gmail.com>
2025-08-31 20:12:16 +0530

    musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+"canibehim": "https://youtu.be/s3j4Mo9iMn4?si=r0f_6KC5rFbWWrFr",
+"perfect": "https://youtu.be/cNGjD0VG4R8?si=gPGCVCJL8MsRPupf",
+"sayyouwon'tletgo" : "https://youtu.be/0yW7w8F2TVA?si=4kH323Lwl2sFjBYZ",
+"obsessed" : "https://youtu.be/FHuWkOkOsQg?si=uvvgHYxsmDpSEJKu",
+"nightchanges" : "https://youtu.be/syFZfO_wfMQ?si=llIHmlNV2K91UaCK",
+"whatmakesyoubeautiful" : "https://youtu.be/QJO3ROT-A4E?si=5gCmZkSFNfKfqKTF",
+}
